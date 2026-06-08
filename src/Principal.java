5e50758a235c1b7742bae2998b6819ccc4daa930
lucapneu <lucaspiovesam07@gmail.com>
2026-06-08 16:16:59 -0300

    Pojeto Finalizado.

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal {
@@ -11,8 +12,10 @@ public class Principal {
         try {
             Endereco novoEndereco = consultaCep.buscaEndereco(cep);
             System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
         }
-        catch (RuntimeException e) {
+        catch (RuntimeException | IOException e) {
             System.out.println(e.getMessage());
             System.out.println("Finalizando o progama");
         }
