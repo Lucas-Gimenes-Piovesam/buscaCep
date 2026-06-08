a0c1865c48aa0f14344060a0d3e7517655f2e0ac
lucapneu <lucaspiovesam07@gmail.com>
2026-06-08 15:46:15 -0300

    Começo do projeto de busca de cep

--- src/ConsultaCep.java
+++ src/ConsultaCep.java
@@ -1,5 +1,6 @@
 import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,9 +15,16 @@ public class ConsultaCep {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(endereco)
                 .build();
-        HttpResponse<String> response = HttpClient
-                .newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         return new Gson().fromJson(response.body(), Endereco.class);
     }
