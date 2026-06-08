5f2a1a35471bdeac77a471cb15116da78a592ac3
lucapneu <lucaspiovesam07@gmail.com>
2026-06-08 16:09:15 -0300

    Implementação de scannerr na classe Principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-
+        Scanner leitura = new Scanner(System.in);
         ConsultaCep consultaCep = new ConsultaCep();
-        Endereco novoEndereco = consultaCep.buscaEndereco("01001000" );
-        System.out.println(novoEndereco);
+
+        System.out.println("Digite um CEP para consultar");
+        var cep = leitura.nextLine();
+
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+        }
+        catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando o progama");
+        }
+
+
+
     }
 }
